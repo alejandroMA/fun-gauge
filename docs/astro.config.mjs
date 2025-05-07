2763d84a30e2bad89763f71a78ef1b1bc30a15aa
Alejandro Madariaga Angeles <amndell@gmail.com>
2025-05-06 20:50:45 -0600

    init docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: 'Fun Gauge',
+            favicon: 'favicon.png',
+            social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/alejandroMA/fun-gauge' },
+                { icon: 'heart', label: 'Alex', href: 'https://alex-ma.dev' }
+            ],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' }
+                    ]
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' }
+                }
+            ]
+        })
+    ]
+})
