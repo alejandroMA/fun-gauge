2d1da76b2cf10a7fbd3d034c7bb2584358fab825
Alejandro Madariaga Angeles <amndell@gmail.com>
2025-07-03 13:52:35 -0600

    docs

--- docs/src/content/docs/react/components/gettingStated.tsx
+++ docs/src/content/docs/react/components/gettingStated.tsx
@@ -9,8 +9,8 @@ export default function MyGauge(props: Props) {
     const { title = 'Hello Gauge', value = 60 } = props
 
     return (
-        <div>
-            <h4>{title}</h4>
+        <div className='flex flex-col items-center bg-[#eff1f5]'>
+            <h4 className='text-[#222]'>{title}</h4>
             <Gauge value={value} />
         </div>
     )
