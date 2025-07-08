51dae53423be98aa6591ba3f669933a863b40fe9
Alejandro Madariaga Angeles <amndell@gmail.com>
2025-07-07 20:48:18 -0600

    home gauges cleanup

--- docs/src/scripts/gaugeUtils.ts
+++ docs/src/scripts/gaugeUtils.ts
@@ -1,10 +1,53 @@
-import type { FunGauge } from '@fun-gauge/core'
+import {
+    type FunGauge,
+    type GaugeThemeProps,
+    type GaugeAnimationProps,
+    type ColorSelector,
+    defaultProps
+} from '@fun-gauge/core'
+import Gauge from '@fun-gauge/core'
 import throttle from 'throttleit'
 
-export function bindResizeObserver(gauge: FunGauge, containerElement: HTMLElement): void {
-    // resize observer
-    // let containerElement = gauge.getCanvasElement().parentElement?.parentElement
-    if (containerElement) {
+export function createHomeGauge(options: {
+    canvasElement: HTMLCanvasElement
+    value: number
+    colorSelectors?: ColorSelector[]
+    animation?: GaugeAnimationProps
+    lightTheme: GaugeThemeProps
+    darkTheme: GaugeThemeProps
+}): FunGauge {
+    let { colorSelectors = defaultProps.colorSelectors, animation = defaultProps.animation } = options
+    let themeName = document.documentElement.dataset.theme
+    let initialTheme = themeName === 'light' ? options.lightTheme : options.darkTheme
+
+    let gauge = Gauge({
+        canvasElement: options.canvasElement,
+        value: options.value,
+        colorSelectors: colorSelectors,
+        animation: animation,
+        theme: initialTheme
+    })
+
+    // React to changes in system color scheme.
+    let themeSelect = document.querySelector('starlight-theme-select')
+    themeSelect?.querySelector('select')?.addEventListener('change', handleThemeChange)
+    matchMedia('(prefers-color-scheme: light)').addEventListener('change', handleThemeChange)
+
+    function handleThemeChange(): void {
+        setTimeout(() => {
+            // get starlight theme after dom update
+            let updatedThemeName = document.documentElement.dataset.theme
+            let theme = updatedThemeName === 'light' ? options.lightTheme : options.darkTheme
+            if (theme) {
+                gauge.updateProps({ theme: theme })
+                gauge.forceRender()
+            }
+        }, 0)
+    }
+
+    // bind resize observer
+    let container = gauge.getCanvasElement()?.parentElement
+    if (container) {
         let resizeGauge = throttle((width) => {
             gauge.updateWidth(Math.floor(width))
             gauge.forceRender()
@@ -13,27 +56,26 @@ export function bindResizeObserver(gauge: FunGauge, containerElement: HTMLElemen
             let width = entries[0].contentRect.width
             resizeGauge(width)
         })
-        observer.observe(containerElement)
+        observer.observe(container)
+    }
+
+    return gauge
+}
+
+export function randSetInterval(callback: () => void, min: number, max: number) {
+    setTimeout(timeoutFunc, randInt(min, max))
+
+    function timeoutFunc() {
+        callback()
+
+        setTimeout(timeoutFunc, randInt(min, max))
     }
 }
 
-export function bindThemeChange(gauge: FunGauge, onThemeChange: (theme: string) => void): void {
-    // React to changes in system color scheme.
-    let themeSelectElement = document.querySelector('starlight-theme-select')
-    themeSelectElement?.querySelector('select')?.addEventListener('change', handleThemeChange)
-    matchMedia('(prefers-color-scheme: light)').addEventListener('change', handleThemeChange)
-
-    function handleThemeChange(): void {
-        setTimeout(() => {
-            // get starlight theme after dom update
-            let theme = document.documentElement.dataset.theme
-            if (theme) {
-                onThemeChange(theme)
-            }
-        }, 0)
-    }
+export function randInt(min: number, max: number): number {
+    return Math.floor(min + Math.random() * (max - min))
 }
 
-export function rand(min: number, max: number): number {
+export function randFloat(min: number, max: number): number {
     return min + Math.random() * (max - min)
 }
